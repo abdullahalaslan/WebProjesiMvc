6f7ec63adaade3c5c91da0d7fd050a7b6fcbadfc
abdullahalaslan <39031447+abdullahalaslan@users.noreply.github.com>
2018-05-31 23:48:15 +0300

    Completed

---
+++ WebApplication2/Controllers/AdminController.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AdminController : Controller
+    {
+        
+        private signEntities3 db = new signEntities3();
+        public ActionResult Index()
+        { 
+            return View();
+        }
+        // GET: Admin
+        public ActionResult UsersIndex()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult UsersDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        // GET: Admin/Create
+        public ActionResult UsersCreate()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UsersCreate([Bind(Include = "Userid,UserName,UserLastName,UserEmail,UserPassword")] Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(users);
+                db.SaveChanges();
+                return RedirectToAction("UsersIndex");
+            }
+
+            return View(users);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult UsersEdit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UsersEdit([Bind(Include = "Userid,UserName,UserLastName,UserEmail,UserPassword")] Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(users).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("UsersIndex");
+            }
+            return View(users);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult UsersDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Users users = db.Users.Find(id);
+            if (users == null)
+            {
+                return HttpNotFound();
+            }
+            return View(users);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("UsersDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult UsersDeleteConfirmed(int id)
+        {
+            Users users = db.Users.Find(id);
+            db.Users.Remove(users);
+            db.SaveChanges();
+            return RedirectToAction("UsersIndex");
+        }
+
+        // HABERLER----------
+        public ActionResult haberIndex()
+        {
+            return View(db.haber.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult haberDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            haber haber = db.haber.Find(id);
+            if (haber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(haber);
+        }
+
+        // GET: Admin/Create
+        public ActionResult haberCreate()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult haberCreate([Bind(Include = "id,baslik,icerik")] haber haber)
+        {
+            if (ModelState.IsValid)
+            {
+                db.haber.Add(haber);
+                db.SaveChanges();
+                return RedirectToAction("haberIndex");
+            }
+
+            return View(haber);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult haberEdit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            haber haber = db.haber.Find(id);
+            if (haber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(haber);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult haberEdit([Bind(Include = "id,baslik,icerik")] haber haber)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(haber).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("haberIndex");
+            }
+            return View(haber);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult haberDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            haber haber = db.haber.Find(id);
+            if (haber == null)
+            {
+                return HttpNotFound();
+            }
+            return View(haber);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("haberDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult haberDeleteConfirmed(int id)
+        {
+            haber haber = db.haber.Find(id);
+            db.haber.Remove(haber);
+            db.SaveChanges();
+            return RedirectToAction("haberIndex");
+        }
+
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+     
+        
+        public ActionResult Haber()
+        {
+            signEntities3 dbc = new signEntities3();
+            haberler k1 = new haberler();
+            k1.habers = dbc.haber.ToList();
+            return View(k1);
+        
+        }
+
+        public ActionResult iletisim()
+        {
+            return View();
+        }
+        public ActionResult Kimdir()
+        {
+            return View();
+        }
+
+        public ActionResult Album()
+        {
+            albumler k = new albumler();
+            k.album = db.albumtik.ToList();
+            return View(k);
+        }
+        public ActionResult albumetik(int? id)
+        {
+            albumtik album = db.albumtik.Find(id);
+            return View(album);
+        }
+       
+        public ActionResult Konser()
+        {
+            return View();
+        }
+        public ActionResult a_index()
+        {
+            return View();
+        }
+        public ActionResult Cikis()
+        {
+            Session["UserName"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        private signEntities3 db = new signEntities3();
+       
+
+
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult register(FormCollection form)
+        {
+            signEntities3 db = new signEntities3();
+            Users model = new Users();
+            model.UserEmail = form["UserEmail"].Trim();
+            model.UserName = form["UserName"].Trim();
+            model.UserLastName = form["UserSurname"].Trim();
+            model.UserPassword = form["UserPassword"].Trim();
+            db.Users.Add(model);
+            db.SaveChanges();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(Users Model, FormCollection form)
+        {
+            var kullanıcı = db.Users.FirstOrDefault(x => x.UserName == Model.UserName && x.UserPassword == Model.UserPassword);
+            string Kul_Adi = form["UserName"].Trim();
+            string Password = form["UserPassword"].Trim();
+            if (Kul_Adi=="admin" && Password=="admin")
+            {
+                return RedirectToAction("a_index","Home");
+            }
+            else if (kullanıcı != null)
+            {
+                Session["UserName"] = kullanıcı;
+                return RedirectToAction("Index", "Home");
+            }
+            Response.Write("<script>alert('Hatalı Giriş')</script>");
+            return View();
+        }
+        public class albumler
+        {
+            public List<albumtik> album { get; set; }
+        }
+        public class haberler
+        {
+            public List<haber> habers { get; set; }
+        }
+    }
+}
